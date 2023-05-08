e09be96d7be3a54c28c4a2d7aa9037b68d8f3a62
JIALIN-HUANG <78190123+JIALIN-HUANG@users.noreply.github.com>
2023-05-08 15:42:25 -0400

    wiki pedia

--- streamlist-test.py
+++ streamlist-test.py
@@ -1,25 +1,15 @@
 import requests
 import streamlit as st
-import tweepy
-
-# Set up API keys and access tokens for Twitter
-TWITTER_API_KEY = 'ZIRswi403CUWgby0MGYBj6X7r'
-TWITTER_API_SECRET = 'ABzoiLlTmjXiXkiW5mLAytNAD6WGaQ3mJW2APhmQ4PvLmyPju7'
-TWITTER_BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAACd3iQEAAAAA42jdPCLcyphYR%2FJRa8UUBMo7pdY%3DkcQ0zV7gftfWPZn7rptsTzBTTQHmNueTX7kxLo8N4r3tvFtQpl'
-
-# Authenticate with Tweepy
-auth = tweepy.AppAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
-api = tweepy.API(auth)
 
 # Wikipedia API base URL
 wiki_url = 'https://en.wikipedia.org/w/api.php'
 
-# Create input fields for search term
-search_term = st.text_input('Enter a search term', 'Python')
+# search - present Python final
+search_term = st.text_input('Enter a search term', 'Python Final')
 
-# Create a button to submit the form
+# Create a button
 if st.button('Search Wikipedia'):
-    # Build the Wikipedia API URL with user input
+    # Wiki input: https://en.wikipedia.org/api/rest_v1/#/
     wiki_params = {
         'action': 'query',
         'format': 'json',
@@ -30,22 +20,4 @@ if st.button('Search Wikipedia'):
     # Query the Wikipedia API and get the response
     response = requests.get(wiki_url, params=wiki_params)
 
-    # Parse the JSON response
     data = response.json()
-
-    # Display the Wikipedia search results
-    st.write(f'Wikipedia search results for "{search_term}":')
-    for item in data['query']['search']:
-        st.write(f'- [{item["title"]}](https://en.wikipedia.org/wiki/{item["title"].replace(" ", "_")})')
-
-# Create a button to search for tweets containing the search term
-if st.button('Search Twitter'):
-    # Query the Twitter API and get the response
-    tweets = api.search_tweets(q=search_term, lang='en', count=2)
-
-    # Display the tweets containing the search term
-    st.write(f'Tweets containing "{search_term}":')
-    for tweet in tweets:
-        user_name = tweet.user.screen_name
-        tweet_text = tweet.text
-        st.write(f'- @{user_name}: {tweet_text}')
